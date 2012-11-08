79f3601ae467b328c904afaa1538bb7dd9b8b767
Orgel <orgel0@freenet.de>
2012-11-08 20:50:39 +0100

    Update 12.10/download/functions/firefox.sh

--- 12.10/download/functions/firefox.sh
+++ 12.10/download/functions/firefox.sh
@@ -1,17 +1,28 @@
 #!/bin/bash 
 
-# --> FireFox Installieren <--
+#######################################################################
+# Title      :    firefox
+# Author     :    Mathias Kraut, Uwe Stark
+# Date       :    2012-11-08
+#######################################################################
+# Description
+#   install Firefox web browser
+# Note:
+#   - 
+#######################################################################
 
+# Required Install Packages
 apt-get install -y --no-install-recommends firefox fluxbox flashplugin-installer eterm
-cd /home/xbmc/
 
-rm -R /home/xbmc/scripts
-
-mkdir scripts
-
-cd scripts
-
-wget http://dl.dropbox.com/u/21136636/firefox.sh
-
-chmod 777 firefox.sh
-
+# Firefox configuration
+if [ -d /home/xbmc/scripts ] ; then
+  rm -R /home/xbmc/scripts
+	mkdir /home/xbmc/scripts
+	wget http://dl.dropbox.com/u/21136636/firefox.sh
+	chmod 777 /home/xbmc/scripts/firefox.sh
+else
+	mkdir /home/xbmc/scripts
+	cd /home/xbmc/scripts
+	wget http://dl.dropbox.com/u/21136636/firefox.sh
+	chmod 777 /home/xbmc/scripts/firefox.sh	
+fi
