bff475df69b06174a879b055630bf85f399cfaf0
Krautmaster <Krautmaster@gmail.com>
2012-11-09 08:36:33 +0100

    add print for makemkv

--- 12.10/download/functions/makemkv.sh
+++ 12.10/download/functions/makemkv.sh
@@ -1,18 +1,18 @@
 #!/bin/bash 
 
-sudo apt-get -y install build-essential libc6-dev libssl-dev libexpat1-dev libgl1-mesa-dev libqt4-dev > /dev/null 2>&1
-cd /usr/src > /dev/null 2>&1
-rm -R makemkv > /dev/null 2>&1
-mkdir makemkv > /dev/null 2>&1
-cd makemkv > /dev/null 2>&1
-wget http://www.makemkv.com/download/makemkv-bin-$MAKEMKV_VERSION.tar.gz > /dev/null 2>&1
-wget http://www.makemkv.com/download/makemkv-oss-$MAKEMKV_VERSION.tar.gz > /dev/null 2>&1
-tar xfvz makemkv-bin-$MAKEMKV_VERSION.tar.gz > /dev/null 2>&1
-tar xfvz makemkv-oss-$MAKEMKV_VERSION.tar.gz > /dev/null 2>&1
-cd makemkv-oss-$MAKEMKV_VERSION > /dev/null 2>&1
+sudo apt-get -y install build-essential libc6-dev libssl-dev libexpat1-dev libgl1-mesa-dev libqt4-dev 
+cd /usr/src 
+rm -R makemkv 
+mkdir makemkv 
+cd makemkv 
+wget http://www.makemkv.com/download/makemkv-bin-$MAKEMKV_VERSION.tar.gz 
+wget http://www.makemkv.com/download/makemkv-oss-$MAKEMKV_VERSION.tar.gz 
+tar xfvz makemkv-bin-$MAKEMKV_VERSION.tar.gz 
+tar xfvz makemkv-oss-$MAKEMKV_VERSION.tar.gz 
+cd makemkv-oss-$MAKEMKV_VERSION 
 echo '#!/bin/bash' > src/ask_eula.sh
 echo 'exit 0' >> src/ask_eula.sh
-make -f makefile.linux > /dev/null 2>&1
+make -f makefile.linux 
 clear
 sudo make -f makefile.linux install 
 cd ../makemkv-bin-$MAKEMKV_VERSION
