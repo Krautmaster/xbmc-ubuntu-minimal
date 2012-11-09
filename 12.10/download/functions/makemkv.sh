44bb733d31bd3d1607f49ac88acccc64742ad010
Krautmaster <Krautmaster@gmail.com>
2012-11-09 09:18:30 +0100

    dont ask for eula on makemkv

--- 12.10/download/functions/makemkv.sh
+++ 12.10/download/functions/makemkv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-MAKEMKV_VERSION = $@
-echo MAKEMKV_VERSION 
+MAKEMKV_VERSION=$1
+echo "$MAKEMKV_VERSION" 
 sudo apt-get -y install build-essential libc6-dev libssl-dev libexpat1-dev libgl1-mesa-dev libqt4-dev 
 cd /usr/src 
 rm -R makemkv 
@@ -12,12 +12,10 @@ wget http://www.makemkv.com/download/makemkv-oss-$MAKEMKV_VERSION.tar.gz
 tar xfvz makemkv-bin-$MAKEMKV_VERSION.tar.gz 
 tar xfvz makemkv-oss-$MAKEMKV_VERSION.tar.gz 
 cd makemkv-oss-$MAKEMKV_VERSION 
-echo '#!/bin/bash' > src/ask_eula.sh
-echo 'exit 0' >> src/ask_eula.sh
 make -f makefile.linux 
-clear
 sudo make -f makefile.linux install 
 cd ../makemkv-bin-$MAKEMKV_VERSION
+echo '#!/bin/bash' > src/ask_eula.sh
+echo 'exit 0' >> src/ask_eula.sh
 make -f makefile.linux
 sudo make -f makefile.linux install
- 
