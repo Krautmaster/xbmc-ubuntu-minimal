162efb4862f9be680c66bbbd20f8453d1522d582
Krautmaster <krautmaster@googlemail.com>
2012-11-11 17:59:38 +0100

    Update 12.10/download/functions/tt_s2_4100.sh

--- 12.10/download/functions/tt_s2_4100.sh
+++ 12.10/download/functions/tt_s2_4100.sh
@@ -1,5 +1,17 @@
 #!/bin/bash 
 
+#######################################################################
+# Title      :    TT S2 4100 TV Card driver
+# Author     :    Mathias Kraut, Uwe Stark
+# Date       :    2012-11-08
+# Version   :	  1.0.0
+#######################################################################
+# Description
+#   compile and install 
+# Note:
+#   - 
+#######################################################################
+
 sudo apt-get -y install build-essential libdigest-sha1-perl linux-headers patchutils libproc-processtable-perl git-core linux-firmware-nonfree
 processtable-perl git
 cd /usr/src
