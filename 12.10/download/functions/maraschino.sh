84ff8f7764af2c7fd2e936f24badd1889358f3fb
Krautmaster <krautmaster@googlemail.com>
2012-11-11 18:00:33 +0100

    Update 12.10/download/functions/maraschino.sh

--- 12.10/download/functions/maraschino.sh
+++ 12.10/download/functions/maraschino.sh
@@ -1,20 +1,22 @@
-#!/bin/bash  
+#!/bin/bash 
 
-# --> MaraschinoSetup  <-
-
+#######################################################################
+# Title      :    Maraschino Setup
+# Author     :    Mathias Kraut, Uwe Stark
+# Date       :    2012-11-08
+# Version   :    1.0.0
+#######################################################################
+# Description
+#   compile and install 
+# Note:
+#   - 
+#######################################################################
 cd /usr/src/
-
 rm -R /opt/maraschino
-
 git clone https://github.com/mrkipling/maraschino.git /opt/maraschino
-
 cp /opt/maraschino/initd /etc/init.d/maraschino
-
 cp /opt/maraschino/default /etc/default/maraschino
-
 chmod a+x /etc/init.d/maraschino
-
 update-rc.d maraschino defaults
-
 /etc/init.d/maraschino start
 
