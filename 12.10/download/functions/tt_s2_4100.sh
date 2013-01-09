f351207adcbc73c54c71e0455f9069dd8ecc2ac3
Krautmaster <krautmaster@googlemail.com>
2013-01-09 22:48:26 +0100

    Update 12.10/download/functions/tt_s2_4100.sh

--- 12.10/download/functions/tt_s2_4100.sh
+++ 12.10/download/functions/tt_s2_4100.sh
@@ -12,7 +12,7 @@
 #   - 
 #######################################################################
 
-sudo apt-get -y install build-essential libdigest-sha1-perl linux-headers patchutils libproc-processtable-perl git-core linux-firmware-nonfree processtable-perl git
+sudo apt-get -y install build-essential patchutils libproc-processtable-perl git-core linux-firmware-nonfree git
 cd /usr/src
 wget http://www.tt-downloads.de/tt_s2_4100_drv_lnx.tar.bz2
 tar xjvf tt_s2_4100_drv_lnx.tar.bz2
